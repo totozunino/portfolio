930d00ec1029fd509863d30d88d9053d81682214
totozunino <mathiaszunino@gmail.com>
2022-03-13 01:12:00 -0300

    feat: updating projects

--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useIntersectionObserver } from "@vueuse/core";
 import { ref, withDefaults, defineProps } from "vue";
+import IconGithub from "@/components/icons/IconGithub.vue";
 
 interface Technology {
   name: string;
@@ -15,14 +16,19 @@ interface ProjectItemProps {
   rightAnimation: string;
   ltr: boolean;
   img: string;
+  source: string;
+  url: string;
+  imgClasses: string;
 }
 
 const props = withDefaults(defineProps<ProjectItemProps>(), {
   ltr: true,
+  imgClasses: "",
 });
 
 const target = ref(null);
 const targetIsVisible = ref(false);
+const showImageOverlay = ref(false);
 
 useIntersectionObserver(target, ([{ isIntersecting }], obvserver) => {
   if (isIntersecting) obvserver.disconnect();
@@ -36,15 +42,54 @@ useIntersectionObserver(target, ([{ isIntersecting }], obvserver) => {
     ref="target"
   >
     <Transition :name="props.leftAnimation">
-      <img
+      <div
+        class="relative z-10 flex items-center justify-center w-full transition-transform sm:w-1/2 hover:scale-105"
         v-show="targetIsVisible"
-        :src="props.img"
-        alt="Avatar App"
-        class="z-10 w-full transition-transform border rounded shadow-lg cursor-pointer sm:w-1/2 dark:border-stone-600 shadow-primary/25 hover:scale-105"
-      />
+        @mouseenter="showImageOverlay = true"
+        @mouseleave="showImageOverlay = false"
+      >
+        <div class="relative">
+          <img
+            :src="props.img"
+            :alt="props.title"
+            class="sm:max-h-[450px] border rounded shadow-lg cursor-pointer dark:border-stone-600 shadow-primary/25"
+            :class="props.imgClasses"
+          />
+          <div
+            v-show="showImageOverlay"
+            class="absolute top-0 left-0 flex items-center justify-center w-full h-full space-y-4"
+            :class="imgClasses ? '' : 'bg-black/40'"
+          >
+            <a v-if="props.url" :href="props.url" target="_blank" class="absolute top-2 right-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                class="fill-white"
+              >
+                <path
+                  d="M 5 3 C 3.9069372 3 3 3.9069372 3 5 L 3 19 C 3 20.093063 3.9069372 21 5 21 L 19 21 C 20.093063 21 21 20.093063 21 19 L 21 12 L 19 12 L 19 19 L 5 19 L 5 5 L 12 5 L 12 3 L 5 3 z M 14 3 L 14 5 L 17.585938 5 L 8.2929688 14.292969 L 9.7070312 15.707031 L 19 6.4140625 L 19 10 L 21 10 L 21 3 L 14 3 z"
+                ></path>
+              </svg>
+            </a>
+            <a
+              v-if="props.source"
+              :href="props.source"
+              target="_blank"
+              class="flex items-center px-2 font-extrabold text-white uppercase bg-black/50 rounded-2xl"
+            >
+              <IconGithub />
+              GitHub
+            </a>
+          </div>
+        </div>
+      </div>
     </Transition>
     <Transition :name="props.rightAnimation">
-      <div :class="props.ltr ? 'text-right' : 'text-left'" v-show="targetIsVisible">
+      <div :class="props.ltr ? 'text-right' : 'text-left'" class="w-full sm:w-1/2" v-show="targetIsVisible">
         <li class="my-5 text-2xl marker:text-primary">{{ props.title }}</li>
         <div
           :class="`${
--- src/components/ProjectsSection.vue
+++ src/components/ProjectsSection.vue
@@ -8,29 +8,14 @@ import TypescriptImg from "@/assets/images/typescript.png";
 import ReactImg from "@/assets/images/react-logo.svg";
 import TailwindImg from "@/assets/images/tailwindcss.svg";
 import FirebaseImg from "@/assets/images/firebase.png";
+import NextImg from "@/assets/images/nextjs.png";
+import CypressImg from "@/assets/images/cypress.png";
+import SeekrImg from "@/assets/images/seekr.png";
+import OnlyCodersImg from "@/assets/images/onlycoders.png";
+import FlutterImg from "@/assets/images/flutter.svg";
+import DartImg from "@/assets/images/dart.png";
 
 const projects = [
-  {
-    title: "Avatar Maker",
-    description: "An app to customize a fun avatar, built in Vue.js, Typescript and Vuetify",
-    technologies: [
-      {
-        name: "Vue.js",
-        img: VueImg,
-      },
-      {
-        name: "Vuetify",
-        img: VuetifyImg,
-      },
-      {
-        name: "Typescript",
-        img: TypescriptImg,
-      },
-    ],
-    leftAnimation: "roll-in-left",
-    rightAnimation: "fade",
-    img: AvatarAppImg,
-  },
   {
     title: "Web Chat",
     description:
@@ -57,36 +42,96 @@ const projects = [
     leftAnimation: "roll-in-left",
     ltr: false,
     img: ChatAppImg,
+    imgClasses: "max-h-full",
+    source: "https://github.com/totozunino/react-chat",
+    url: "",
   },
-  // {
-  //   title: "Only Coders",
-  //   description:
-  //     "Social network mobile app focused on technology for it to sponsor as a facilitator in the interconnection of professionals of the area.",
-  //   technologies: [
-  //     {
-  //       name: "Flutter",
-  //       img: "flutter.svg",
-  //     },
-  //     {
-  //       name: "Dart",
-  //       img: "dart.png",
-  //     },
-  //     {
-  //       name: "Firebase",
-  //       img: "firebase.png",
-  //     },
-  //   ],
-  //   rightAnimation: "fade",
-  //   leftAnimation: "roll-in-left",
-  //   img: "onlycoders.png",
-  // },
+  {
+    title: "Seekr Search",
+    description:
+      "A web app offerings a search engine, news search, and information evaluation through Seekr Score and Political Lean Indicator.",
+    technologies: [
+      {
+        name: "Next.js",
+        img: NextImg,
+      },
+      {
+        name: "Tailwind",
+        img: TailwindImg,
+      },
+      {
+        name: "Cypress",
+        img: CypressImg,
+      },
+      {
+        name: "Typescript",
+        img: TypescriptImg,
+      },
+    ],
+    rightAnimation: "fade",
+    leftAnimation: "roll-in-left",
+    img: SeekrImg,
+    imgClasses: "max-h-full",
+    source: "",
+    url: "https://beta.seekr.com/",
+  },
+  {
+    title: "Only Coders",
+    description:
+      "Social network mobile app focused on technology for it to sponsor as a facilitator in the interconnection of professionals of the area.",
+    technologies: [
+      {
+        name: "Flutter",
+        img: FlutterImg,
+      },
+      {
+        name: "Dart",
+        img: DartImg,
+      },
+      {
+        name: "Firebase",
+        img: FirebaseImg,
+      },
+    ],
+    rightAnimation: "fade",
+    leftAnimation: "roll-in-left",
+    ltr: false,
+    img: OnlyCodersImg,
+    imgClasses: "border-none shadow-none max-h-[242px]",
+    source: "https://github.com/Only-Coders/mobile",
+    url: "",
+  },
+  {
+    title: "Avatar Maker",
+    description: "An app to customize a fun avatar, built in Vue.js, Typescript and Vuetify",
+    technologies: [
+      {
+        name: "Vue.js",
+        img: VueImg,
+      },
+      {
+        name: "Vuetify",
+        img: VuetifyImg,
+      },
+      {
+        name: "Typescript",
+        img: TypescriptImg,
+      },
+    ],
+    leftAnimation: "roll-in-left",
+    rightAnimation: "fade",
+    img: AvatarAppImg,
+    imgClasses: "max-h-full",
+    source: "https://github.com/totozunino/avatar-maker",
+    url: "https://avatar-maker-2ba67.web.app/",
+  },
 ];
 </script>
 
 <template>
   <section id="projects" class="h-full p-4">
-    <div class="flex flex-col items-center max-w-4xl mx-auto">
-      <h1 class="mt-10 text-4xl">Projects</h1>
+    <h1 class="flex flex-col items-center max-w-4xl mx-auto mt-10 text-4xl">Projects</h1>
+    <div class="flex flex-col items-center max-w-4xl mx-auto space-y-32">
       <ProjectItem v-for="project in projects" :key="project.title" v-bind="{ ...project }"></ProjectItem>
     </div>
   </section>
